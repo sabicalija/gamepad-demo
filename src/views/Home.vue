79601710682ecf13d2dc6888f149f1676b9e525d
Alija Sabic <sabic.alija@gmail.com>
2020-02-16 22:45:08 +0100

    Initial commit: setup vue, router, store; gamepad selection, basic axes and button display

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <GamePadSelection />
+    <GamePad />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import GamePadSelection from "@/components/GamePadSelection.vue";
+import GamePad from "@/components/GamePad.vue";
+
+export default {
+  name: "Home",
+  components: {
+    GamePadSelection,
+    GamePad
+  }
+};
+</script>
