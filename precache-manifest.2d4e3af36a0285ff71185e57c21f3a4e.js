f5a6cbfd986983ac71ddfbc0363e88b1f8370c63
Alija Sabic <sabic.alija@gmail.com>
2020-02-17 01:22:56 +0100

    Initial release

---
+++ precache-manifest.2d4e3af36a0285ff71185e57c21f3a4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d2286b491ae725ea759",
+    "url": "/css/app.9d1d4998.css"
+  },
+  {
+    "revision": "217c7faf19f2a011e31f54445670e495",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8940fc7f5cb7cbfacf8",
+    "url": "/js/about.b98d2a83.js"
+  },
+  {
+    "revision": "6d2286b491ae725ea759",
+    "url": "/js/app.43fd7442.js"
+  },
+  {
+    "revision": "6d04d174085eaf305acb",
+    "url": "/js/chunk-vendors.eee092e7.js"
+  },
+  {
+    "revision": "549fe8440231ca390bdc7d4675854f21",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ precache-manifest.47ae37a3e3b5928f80004b8d68a69073.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f16ec6bb0cb394d6919",
+    "url": "/gamepad-demo/css/app.ad402700.css"
+  },
+  {
+    "revision": "93eb11db894200fba24a6f88e8491504",
+    "url": "/gamepad-demo/index.html"
+  },
+  {
+    "revision": "d8940fc7f5cb7cbfacf8",
+    "url": "/gamepad-demo/js/about.b98d2a83.js"
+  },
+  {
+    "revision": "3f16ec6bb0cb394d6919",
+    "url": "/gamepad-demo/js/app.590d985f.js"
+  },
+  {
+    "revision": "6d04d174085eaf305acb",
+    "url": "/gamepad-demo/js/chunk-vendors.eee092e7.js"
+  },
+  {
+    "revision": "549fe8440231ca390bdc7d4675854f21",
+    "url": "/gamepad-demo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/gamepad-demo/robots.txt"
+  }
+]);
