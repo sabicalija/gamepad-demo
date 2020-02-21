89852628d2d5ea53f640b0983eadb8b4a664510c
Alija Sabic <sabic.alija@gmail.com>
2020-02-21 22:26:29 +0100

    Automate favicons and manifest generation (window, linux, macos, android, firefox)

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,40 @@ module.exports = {
       options[0].title = "GamePad API Demo";
       return options;
     });
+    config
+      .plugin("favicon")
+      .before("html")
+      .use(require("favicons-webpack-plugin"), [
+        {
+          logo: "@/assets/logo-central.svg",
+          cache: true,
+          devMode: "webapp",
+          prefix: "img/favicon/",
+          inject: "force",
+          favicons: {
+            appName: "GamePad API Demo",
+            appDescription: "GamePad API Demo made with Vue.js",
+            theme_color: "#4DBA87",
+            background: "#ffc40d",
+            appleStatusBarStyle: "black-translucent",
+            dir: "auto",
+            lang: "en-US",
+            version: "1.0",
+            display: "standalone",
+            orientation: "any",
+            start_url: "/gamepad-demo/",
+            icons: {
+              android: true, // Create Android homescreen icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+              appleIcon: true, // Create Apple touch icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+              appleStartup: true, // Create Apple startup images. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+              coast: false, // Create Opera Coast icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+              favicons: true, // Create regular favicons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+              firefox: true, // Create Firefox OS icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+              windows: true, // Create Windows 8 tile icons. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+              yandex: false // Create Yandex browser icon. `boolean` or `{ offset, background, mask, overlayGlow, overlayShadow }`
+            }
+          }
+        }
+      ]);
   }
 };
