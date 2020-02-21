80250974590b2de8f12db97f3d5208e4adc5442b
Alija Sabic <sabic.alija@gmail.com>
2020-02-21 01:38:51 +0100

    Inject global stylus style and variables

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  publicPath: "/gamepad-demo/"
+  publicPath: "/gamepad-demo/",
+  css: {
+    loaderOptions: {
+      stylus: {
+        import: ["~@/style/variables.styl", "~@/style/index.styl"]
+      }
+    }
+  },
+  chainWebpack: config => {
+    config.plugin("html").tap(options => {
+      options[0].title = "GamePad API Demo";
+      return options;
+    });
+  }
 };
