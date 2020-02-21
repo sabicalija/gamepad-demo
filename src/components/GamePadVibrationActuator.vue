c343f2fc993d5f712ce07b48db9094c54d62eba9
Alija Sabic <sabic.alija@gmail.com>
2020-02-21 01:37:06 +0100

    Create component displaying the gamepad vibration actuators

---
+++ src/components/GamePadVibrationActuator.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="gamepad-vibration-actuator">
+    <div>
+      <span>Type</span>
+      <span>{{ actuator ? actuator.type : "" }}</span>
+      <button @click="vibrate" :disabled="active">Start</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "GamePadVibrationActuator",
+  data() {
+    return {
+      actuator: null,
+      active: false
+    };
+  },
+  computed: mapGetters(["gamepad"]),
+  methods: {
+    update() {
+      if (this.gamepad) {
+        const gp = navigator.getGamepads()[this.gamepad.index];
+        this.actuator = gp.vibrationActutator;
+      }
+    },
+    tick() {
+      this.update();
+      window.requestAnimationFrame(this.tick);
+    },
+    vibrate() {
+      if (this.gamepad) {
+        console.log("vibration");
+        const gp = navigator.getGamepads()[this.gamepad.index];
+        this.active = true;
+        const duration = 400;
+        gp.vibrationActuator.playEffect("dual-rumble", {
+          startDelay: 0,
+          duration,
+          weakMagnitude: 0.45,
+          strongMagnitude: 0.75
+        });
+        setTimeout(() => {
+          this.active = false;
+        }, duration);
+      }
+    }
+  },
+  created() {
+    window.requestAnimationFrame(this.tick);
+  }
+};
+</script>
+
+<style lang="stylus" scoped></style>
