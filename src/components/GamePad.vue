ef35c19c26c126de1a2c066bbd94a2fcf6850fb7
Alija Sabic <sabic.alija@gmail.com>
2020-02-21 01:39:46 +0100

    Refactor gamepad: replace draft with new components

--- src/components/GamePad.vue
+++ src/components/GamePad.vue
@@ -1,69 +1,20 @@
 <template>
   <div id="gamepad">
     <GamePadInfo />
-    <div id="axes">
-      <span>Axes:</span>
-      <ul>
-        <li v-for="(axe, i) of axes" :key="i">{{ axe }}</li>
-      </ul>
-    </div>
-    <div id="buttons">
-      <span>Buttons:</span>
-      <ul>
-        <li v-for="(button, i) of buttons" :key="i">Button {{ i }}: {{ button.value }}</li>
-      </ul>
-    </div>
-    <div id="vibration">
-      <span>Vibration Actuator:</span>
-      <div v-if="vActuator">
-        <span>Type: {{ vActuator.type }}</span>
-      </div>
-    </div>
+    <GamePadAxes />
+    <GamePadButtons />
+    <GamePadVibrationActuator />
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
 import GamePadInfo from "@/components/GamePadInfo.vue";
+import GamePadAxes from "@/components/GamePadAxes.vue";
+import GamePadButtons from "@/components/GamePadButtons.vue";
+import GamePadVibrationActuator from "@/components/GamePadVibrationActuator.vue";
 export default {
   name: "GamePad",
-  components: { GamePadInfo },
-  data() {
-    return {
-      axes: null,
-      buttons: null,
-      vActuator: null
-    };
-  },
-  computed: mapGetters(["gamepad"]),
-  methods: {
-    updateGamePad() {
-      if (!this.gamepad) {
-        this.axes = [];
-        this.buttons = [];
-        this.vActuator = null;
-      } else {
-        const { index } = this.gamepad;
-        const gamepads = navigator.getGamepads();
-        const gamepad = gamepads[index];
-        this.axes = gamepad.axes;
-        this.buttons = gamepad.buttons;
-        this.vActuator = gamepad.vibrationActuator;
-      }
-    },
-    tick() {
-      this.updateGamePad();
-      window.requestAnimationFrame(this.tick);
-    }
-  },
-  watch: {
-    selection() {
-      this.updateGamePad();
-    }
-  },
-  created() {
-    window.requestAnimationFrame(this.tick);
-  }
+  components: { GamePadInfo, GamePadAxes, GamePadButtons, GamePadVibrationActuator }
 };
 </script>
 
