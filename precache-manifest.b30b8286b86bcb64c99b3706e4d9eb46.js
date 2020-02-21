e9888b3749f1be5c2a144abec72c908a101a2778
Alija Sabic <sabic.alija@gmail.com>
2020-02-21 13:56:40 +0100

    Release (v0.5)
    feat:
      info: visualize joysticks

---
+++ precache-manifest.b30b8286b86bcb64c99b3706e4d9eb46.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "560e9301635b10d82d4e",
+    "url": "/gamepad-demo/css/app.07475511.css"
+  },
+  {
+    "revision": "2c287c905b3ee89aef653c052f903254",
+    "url": "/gamepad-demo/img/logo-2.svg"
+  },
+  {
+    "revision": "6c91599b9b1651cfb80a1c0d5eaf7d06",
+    "url": "/gamepad-demo/img/logo.svg"
+  },
+  {
+    "revision": "8c9307073346ce21bdd38d627333d039",
+    "url": "/gamepad-demo/index.html"
+  },
+  {
+    "revision": "cea37ad5e4727e616232",
+    "url": "/gamepad-demo/js/about.0a80adfe.js"
+  },
+  {
+    "revision": "560e9301635b10d82d4e",
+    "url": "/gamepad-demo/js/app.8822eae8.js"
+  },
+  {
+    "revision": "478111be4553ed865f07",
+    "url": "/gamepad-demo/js/chunk-vendors.c3f90ad3.js"
+  },
+  {
+    "revision": "549fe8440231ca390bdc7d4675854f21",
+    "url": "/gamepad-demo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/gamepad-demo/robots.txt"
+  }
+]);
