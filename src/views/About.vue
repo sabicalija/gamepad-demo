5429fc5da1881e24c8489aed0441b0df0d662539
Alija Sabic <sabic.alija@gmail.com>
2020-02-21 17:42:46 +0100

    Create simple about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div id="about">
+    <h1>GamePad API made with Vue.js.</h1>
+    <p>
+      This demo shows how to integrate the HTML5 Gamepad API in a Vue.js application. Features
+      included:
+      <ul>
+        <li>vue-router</li>
+        <li>vuex</li>
+        <li>webpack</li>
+        <li>fontawesome</li>
+      </ul>
+    </p>
   </div>
 </template>
+
+<style lang="stylus" scoped></style>
